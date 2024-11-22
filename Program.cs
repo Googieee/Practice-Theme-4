2f712ced125072edb815f71b1c9349d51bc3c677
Googieee <utuznikov96@gmail.com>
2024-11-23 01:56:45 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿try
+{
+    Console.WriteLine("Введите делимое:");
+    int dividend = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите делитель:");
+    int divisor = Convert.ToInt32(Console.ReadLine());
+
+    int result = dividend / divisor;
+    Console.WriteLine($"Результат деления: {result}");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("Ошибка: Деление на ноль недопустимо.");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Ошибка: Пожалуйста, введите корректное число.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Неизвестная ошибка: {ex.Message}");
+}
+finally
+{
+    Console.WriteLine("Программа завершила работу. Нажмите любую клавишу для выхода.");
+    Console.ReadKey();
+}
+    
